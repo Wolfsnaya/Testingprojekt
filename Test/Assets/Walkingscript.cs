b25f34f9861154064440450234834cd6c897464a
Wolfsnaya <128799658+Wolfsnaya@users.noreply.github.com>
2024-12-13 17:44:04 +0100

    Player Walking
    Walking + Kamera follow

--- Test/Assets/Walkingscript.cs
+++ Test/Assets/Walkingscript.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class Walkingscript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform playerCamera;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 180f;
+    public float cameraRotationSpeed = 2f;
+    public float cameraFollowDistance = 5f;
+
+    private void Update()
     {
-        
+        HandlePlayerMovement();
+        HandleCameraRotation();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandlePlayerMovement()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+
+        float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotation);
+    }
+
+    private void HandleCameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        playerCamera.Rotate(Vector3.up, mouseX * cameraRotationSpeed);
+    }
+
+    private void LateUpdate()
+    {
+        // Calculate the desired camera position based on player's position and camera offset
+        Vector3 desiredPosition = transform.position - transform.forward * cameraFollowDistance + Vector3.up * 2f;
         
+        // Smoothly move the camera towards the desired position
+        playerCamera.position = Vector3.Lerp(playerCamera.position, desiredPosition, Time.deltaTime * 10f);
+
+        // Make the camera always look at the player
+        playerCamera.LookAt(transform.position + transform.forward * cameraFollowDistance);
     }
 }
